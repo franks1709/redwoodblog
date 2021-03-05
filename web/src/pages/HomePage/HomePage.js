c145dff4743ad1a24394b16b2639de7fe6c2f9f7
franks1709 <frank.schlegge@t-online.de>
2021-03-05 18:55:27 +0100

    Implement contact

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,8 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return <BlogLayout>Home</BlogLayout>
+  return (
+    <BlogLayout>
+      <BlogPostsCell />
+    </BlogLayout>
+  )
 }
 
 export default HomePage
